cc674cea88b0bcef37fad7a68bb9ae1473ee36b5
Tanner Helton <tanner@tannerhelton.com>
2022-11-08 08:03:52 -0600

    debug value

--- Quiz.php
+++ Quiz.php
@@ -1,14 +1,14 @@
 <html>
     <body>
+        <h1>Questions and Answers:</h1>
         <?php
         $q1 = $_POST["1"];
         $q2 = $_POST["2"];
-        echo "<p>Questions and Answers:</p>";
         echo "<br>Question 1: What's the acronym for HyperText Markup Language";
-        echo "<br>     You answered: "+$q1;
+        echo "<br>     You answered: ".$q1;
         echo "<br>     Correct answer: HTML";
         echo "<br>Question 2: What language is being used for this form?";
-        echo "<br>     You answered: "+$q2;
+        echo "<br>     You answered: ".$q2;
         echo "<br>     Correct answer: PHP";
         ?>
     </body>
