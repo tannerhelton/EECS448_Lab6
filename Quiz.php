5a2d131ecc049bf186e10f5011c3f6660828c8e5
Tanner Helton <tanner@tannerhelton.com>
2022-11-07 20:28:35 -0600

    updating for form

--- Quiz.php
+++ Quiz.php
@@ -1,4 +1,12 @@
 <?php
 $q1 = $_POST["1"];
 $q2 = $_POST["2"];
+
+echo "<p>Questions and Answers:</p>";
+echo "<br>Question 1: What's the acronym for HyperText Markup Language";
+echo "<br>&nbsp;You answered: "+$q1;
+echo "<br>&nbsp;Correct answer: HTML";
+echo "<br>Question 2: What language is being used for this form?";
+echo "<br>&nbsp;You answered: "+$q2;
+echo "<br>&nbsp;Correct answer: PHP";
 ?>
