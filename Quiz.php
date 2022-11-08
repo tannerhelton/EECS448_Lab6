c9861145e4e10c536b208ce9aaf1c72b4b7be32d
Tanner Helton <tanner@tannerhelton.com>
2022-11-08 08:00:29 -0600

    part 2

--- Quiz.php
+++ Quiz.php
@@ -1,4 +1,15 @@
-<?php
-$q1 = $_POST["1"];
-$q2 = $_POST["2"];
-?>
+<html>
+    <body>
+        <?php
+        $q1 = $_POST["1"];
+        $q2 = $_POST["2"];
+        echo "<p>Questions and Answers:</p>";
+        echo "<br>Question 1: What's the acronym for HyperText Markup Language";
+        echo "<br>     You answered: "+$q1;
+        echo "<br>     Correct answer: HTML";
+        echo "<br>Question 2: What language is being used for this form?";
+        echo "<br>     You answered: "+$q2;
+        echo "<br>     Correct answer: PHP";
+        ?>
+    </body>
+</html>
